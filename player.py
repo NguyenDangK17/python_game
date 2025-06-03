692ddc1fbd0e66c582e231cd9e3bf387290dbf92
Nguyen Dang <dangnse173237@fpt.edu.vn>
2025-06-03 16:00:15 +0700

    Destroy Asteroid

--- player.py
+++ player.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from shot import Shot
 
 class CircleShape(pygame.sprite.Sprite):
     def __init__(self, x, y, radius):
@@ -19,6 +20,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shoot_timer = 0
 
         for group in Player.containers:
             group.add(self)
@@ -40,6 +42,16 @@ class Player(CircleShape):
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
+        
+    def shoot(self):
+        if self.shoot_timer > 0:
+            return
+        
+        direction = pygame.Vector2(0, 1).rotate(self.rotation)
+        velocity = direction * PLAYER_SHOOT_SPEED
+        Shot(self.position.x, self.position.y, velocity)
+        
+        self.shoot_timer = PLAYER_SHOOT_COOLDOWN
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
@@ -52,3 +64,8 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
+            
+        if self.shoot_timer > 0:
+            self.shoot_timer -= dt
---
+++ shot.py
@@ -0,0 +1,18 @@
+import pygame
+from constants import *
+from asteroid import CircleShape
+
+class Shot(CircleShape):
+    containers = ()
+
+    def __init__(self, x, y, velocity):
+        super().__init__(x, y, SHOT_RADIUS)
+        self.velocity = velocity
+        for group in self.containers:
+            group.add(self)
+
+    def update(self, dt):
+        self.position += self.velocity * dt
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", self.position, self.radius)
