eee4c8c10caddc55ff167787a9e4b817065a8a41
Nguyen Dang <dangnse173237@fpt.edu.vn>
2025-06-03 12:05:25 +0700

    Add Player

--- main.py
+++ main.py
@@ -1,25 +1,30 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-    
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+
+    clock = pygame.time.Clock()
+    dt = 0
+
+    # Create player in the center of the screen
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-    screen.fill((0, 0, 0))
-    pygame.display.flip()
+        player.update(dt)
+
+        screen.fill((0, 0, 0))
+
+        player.draw(screen)
+
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
-    
